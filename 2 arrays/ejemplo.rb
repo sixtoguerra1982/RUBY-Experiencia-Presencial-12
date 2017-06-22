8d0d5c6977c60463a9e814d7451cbf07c662d34f
sixtoguerra1982 <sixto.guerra1982@gmail.com>
2017-06-22 01:18:30 -0400

    arreglos 21062017

---
+++ 2 arrays/ejemplo.rb
@@ -0,0 +1,3 @@
+opposites = {positive: "negative", up: "down", right: "left"}
+opposites.each { |key| puts key}
+opposites.each { |value| puts value}
--- 2 arrays/ejercicio2.rb
+++ 2 arrays/ejercicio2.rb
@@ -1,4 +1,5 @@
 # Dado el array:
+# a = [1,2,3,9,1,4,5,2,3,6,6]
 # 1. Eliminar el último elemento.
 # 2. Eliminar el primer elemento.
 # 3. Eliminar el elemento que se encuentra en la posición media,
@@ -9,4 +10,31 @@
 # 5. Utilizando un arreglo vacío auxiliar y operaciones de push y pop:
 #    invertir el orden de los elementos en un arreglo.
 
+
+
+
+#eliminar el ultimo elemento
 a = [1,2,3,9,1,4,5,2,3,6,6]
+
+a.pop(1)
+#
+
+#Eliminar el primer elemento.
+a.shift
+print a
+print "\n"
+#
+
+# 3. Eliminar el elemento que se encuentra en la posición media,
+#    si el arreglo tiene un número par de elementos entonces remover
+#    el que se encuentre en la mitad izquierda, ejemplo:
+#    en el arreglo [1,2,3,4] se removería el elemento 2.
+pos_media = ((a.length).to_f) / 2
+pos_media += 0.5 if pos_media != 0 && pos_media % 2 != 0
+a.delete_at(pos_media-1)
+print a 
+#
+
+# 4. Borrar el último elemento mientras ese número sea distinto a 1.
+a.delete_at(-1) if a[-1] != 1
+
